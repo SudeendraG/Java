3b6d162b8374aa7de2d2c1c031dabaf608683748
sudeendrag <sudeendrag@dhi-sudeendra.digitalharbor.com>
2019-05-23 12:13:10 +0530

    Predicate Example

--- src/com/sudeendrag/java8/PredicateExample.java
+++ src/com/sudeendrag/java8/PredicateExample.java
@@ -6,13 +6,16 @@ import com.sudeendrag.corejava.collections.Employee;
 
 /*
 The Functional Interface PREDICATE is defined in the java.util.Function
-package.It improves manageability of code,
+package.It improves readability of code,
 helps in unit-testing them separately,
-We can Replace Conditional Checks with predicates..*/
-public class PredicateExample1 {
+We can Replace Conditional Checks with predicates..
+return : boolean
+*/
+public class PredicateExample {
 	
 	public static void main(String[] args) {
 		Predicate<Integer> p=i->i%2==0;
+		Predicate<Integer> p1=i->i>10;
 		System.out.println(p.test(10));
 		System.out.println(p.test(7));
 		
@@ -23,5 +26,20 @@ public class PredicateExample1 {
 		e2.setSalary(200000);
 		System.out.println(p2.test(e1));
 		System.out.println(p2.test(e2));
+		
+		// Predicate Joining  and, or , negate
+		
+		System.out.println(p.and(p1).test(100));
+		System.out.println(p.and(p1).test(8));
+		
+		System.out.println(p.or(p1).test(9));
+		System.out.println(p.or(p1).test(12));
+		
+		
+		System.out.println(p.negate().test(10));
+		System.out.println(p.negate().test(7));
+	
 	}
+	
+	// BiPredicate
 }
